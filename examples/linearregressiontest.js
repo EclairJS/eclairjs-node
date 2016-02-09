3f7f20b99463fb86e832e5edac41a2ef5836949e
Doron Rosenberg <doronr@us.ibm.com>
2016-02-09 15:54:54 -0800

    #33- start of implementing mllib.  Implement enough to get our basic linear regression model working.

---
+++ examples/linearregressiontest.js
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2016 IBM Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+function exit() {
+  process.exit();
+}
+
+var spark = require('../lib/index.js');
+
+var sc = new spark.SparkContext("local[*]", "Linear Regression Test");
+
+var data = sc.textFile(__dirname + "/lpsa.txt");
+
+var parsedData = data.map( function(s) {
+  var parts = s.split(",");
+  var features = parts[1].split(" ");
+  return new LabeledPoint(parts[0], new DenseVector(features));
+});
+
+var numIterations = 3;
+var linearRegressionModel = spark.mllib.regression.LinearRegressionWithSGD.train(parsedData, numIterations);
+
+var delta = 17;
+var valuesAndPreds = parsedData.mapToPair(function(lp, linearRegressionModel1) { // FIXME
+  var label = lp.getLabel();
+  var f = lp.getFeatures();
+  var prediction = linearRegressionModel1.predict(f) + 17;
+  return [prediction, label];
+
+}); // end MapToPair
+
+valuesAndPreds.take(10).then(function(results) {
+  console.log(results);
+  sc.stop().then(exit).catch(exit);
+}).catch(function(e) {
+  console.log(e)
+  sc.stop().then(exit).catch(exit);
+});
