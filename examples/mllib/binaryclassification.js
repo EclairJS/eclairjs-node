58fe7341ef94d87180bc46894575245c2c9393f1
Doron Rosenberg <doronr@us.ibm.com>
2016-02-12 15:14:41 -0800

    implement binary classification mllib example

---
+++ examples/mllib/binaryclassification.js
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2016 IBM Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+function exit() {
+  process.exit();
+}
+
+function stop(e) {
+  if (e) {
+    console.log(e);
+  }
+  sc.stop().then(exit).catch(exit);
+}
+
+var spark = require('../../lib/index.js');
+
+var sc = new spark.SparkContext("local[*]", "Binary Classification Metrics Test");
+
+var data = spark.mllib.utils.MLUtils.loadLibSVMFile(sc, __dirname + "/sample_binary_classification_data.txt");
+
+// Split data into training (60%) and test (40%)
+data.randomSplit([0.6, 0.4], 11).then(function(split) {
+  //var training = split[0].cache();
+  var training = split[0];
+  var test = split[1];
+
+  var model = new spark.mllib.classification.LogisticRegressionWithLBFGS()
+    .setNumClasses(2)
+    .run(training);
+
+  var predictionAndLabels = test.mapToPair(function(lp, model) {
+    return [model.predict(lp.getFeatures()), lp.getLabel()];
+  }, [model]);
+
+  var metrics = new spark.mllib.evaluation.BinaryClassificationMetrics(predictionAndLabels);
+
+  var promises = [];
+
+  promises.push(metrics.precisionByThreshold().collect());
+  promises.push(metrics.recallByThreshold().collect());
+  promises.push(metrics.fMeasureByThreshold().collect());
+  promises.push(metrics.fMeasureByThreshold(2.0).collect());
+  promises.push(metrics.pr().collect());
+
+  Promise.all(promises).then(function(results) {
+    console.log("Precision By Threshold:", results[0]);
+    console.log("Recall By Threshold:", results[1]);
+    console.log("Measure By Threshold:", results[2]);
+    console.log("Measure By Threshold (Beta of 2.0):", results[3]);
+    console.log("Precision-recall curve:", results[3]);
+
+    stop();
+  }).catch(stop);
+}).catch(stop);
