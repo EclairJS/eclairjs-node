59e83123adbb56ec81ec39978866c6984f1be20a
Doron Rosenberg <doronr@us.ibm.com>
2016-02-22 15:36:32 -0800

    fix #38 - Implement mllib decision tree classification

---
+++ examples/mllib/decisiontreeclassification.js
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2016 IBM Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+function exit() {
+  process.exit();
+}
+
+function stop(e) {
+  if (e) {
+    console.log(e);
+  }
+  sc.stop().then(exit).catch(exit);
+}
+
+var spark = require('../../lib/index.js');
+
+var sc = new spark.SparkContext("local[*]", "Decision Tree Classificatio");
+
+var data = spark.mllib.utils.MLUtils.loadLibSVMFile(sc, __dirname + "/sample_libsvm_data.txt");
+
+data.randomSplit([0.7, 0.3]).then(function(splits) {
+  var trainingData = splits[0];
+  var testData = splits[1];
+
+  // Set parameters.
+  //  Empty categoricalFeaturesInfo indicates all features are continuous.
+  var numClasses = 2;
+  var categoricalFeaturesInfo = {};
+  var impurity = "gini";
+  var maxDepth = 5;
+  var maxBins = 32;
+
+  // Train a DecisionTree model for classification.
+  var model = spark.mllib.tree.DecisionTree.trainClassifier(trainingData, numClasses, categoricalFeaturesInfo, impurity, maxDepth, maxBins);
+
+  // Evaluate model on test instances and compute test error
+  var predictionAndLabel = testData.mapToPair(function (labeledPoint, model) {
+    return [model.predict(labeledPoint.getFeatures()), labeledPoint.getLabel()];
+  }, [model]);
+
+  var result = predictionAndLabel.filter(function (tuple2) {
+    return (tuple2[0] != tuple2[1]);
+  });
+
+  var promises = [];
+
+  promises.push(result.count());
+  promises.push(testData.count());
+
+  Promise.all(promises).then(function(results) {
+    console.log("Test Error:", results[0] / results[1])
+  }).catch(stop);
+}).catch(stop);
+
