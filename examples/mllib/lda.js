d695df1ec9a4ab4cd602274b05e29e25ae0ba038
Doron Rosenberg <doronr@us.ibm.com>
2016-03-14 13:34:54 -0700

    Fix #51 - Implement mllib.LDA Example

---
+++ examples/mllib/lda.js
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2016 IBM Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+function exit() {
+  process.exit();
+}
+
+function stop(e) {
+  if (e) {
+    console.log(e);
+  }
+  sc.stop().then(exit).catch(exit);
+}
+
+function createResulPromise(label, promise) {
+  return new Promise(function(resolve, reject) {
+    promise.then(function(result) {
+      resolve([label, result])
+    }).catch(reject);
+  });
+}
+
+
+var spark = require('../../lib/index.js');
+
+var sc = new spark.SparkContext("local[*]", "LDA Example");
+
+var data = sc.textFile(__dirname + "/data/sample_lda_data.txt");
+
+var parsedData = data.map(function (s) {
+  var sarray = s.trim().split(" ");
+  var values = [];
+  for (var i = 0; i < sarray.length; i++) {
+    values[i] = parseFloat(sarray[i]);
+  }
+  return Vectors.dense(values);
+});
+
+// Index documents with unique IDs
+var zipIndex = parsedData.zipWithIndex().map(function (doc_id) {
+  return new Tuple(doc_id[1], doc_id[0]); // swap
+});
+
+var corpus = spark.rdd.PairRDD.fromRDD(zipIndex).cache();
+
+// Cluster the documents into three topics using LDA
+var ldaModel = new spark.mllib.clustering.LDA().setK(3).run(corpus);
+
+var promises = [];
+
+// Output topics. Each is a distribution over words (matching word count vectors)
+promises.push(createResulPromise('Vocab Size', ldaModel.vocabSize()));
+
+promises.push(createResulPromise('Topics Matrix', ldaModel.topicsMatrix().toArray()));
+
+Promise.all(promises).then(function(results) {
+  results.forEach(function(result) {
+    console.log(result[0], '=', result[1])
+  });
+
+  stop();
+}).catch(stop);
