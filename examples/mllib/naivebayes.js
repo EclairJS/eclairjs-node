ce5456a494da42620f64b36544c69aecb57728dc
Doron Rosenberg <doronr@us.ibm.com>
2016-03-18 10:11:51 -0700

    #55 - Implement mllib NaiveBayes Example

---
+++ examples/mllib/naivebayes.js
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2016 IBM Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+function exit() {
+  process.exit();
+}
+
+function stop(e) {
+  if (e) {
+    console.log(e);
+  }
+  sc.stop().then(exit).catch(exit);
+}
+
+var spark = require('../../lib/index.js');
+
+var sc = new spark.SparkContext("local[*]", "Naive Bayes Example");
+
+var data = spark.mllib.util.MLUtils.loadLibSVMFile(sc, __dirname + "/data/sample_libsvm_data.txt");
+
+data.randomSplit([0.6, 0.4], 12345).then(function(splits) {
+  var trainingData = splits[0];
+  var testData = splits[1];
+
+  var model = spark.mllib.classification.NaiveBayes.train(trainingData, 1.0);
+
+  var predictionAndLabel = testData.mapToPair(function (lp, model) {
+    return new Tuple(model.predict(lp.getFeatures()), lp.getLabel());
+  }, [model]);
+
+  var filtered = predictionAndLabel.filter(function (tuple) {
+    return tuple[0] == tuple[1];
+  });
+
+  var promises = [];
+
+  promises.push(filtered.count());
+  promises.push(testData.count());
+
+  Promise.all(promises).then(function(results) {
+    console.log('Accuracy:', results[0]/results[1]);
+    stop();
+  }).catch(stop);
+}).catch(stop);
+
