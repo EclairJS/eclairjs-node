d4ba33cd054fdf6abe7134dfc6d4a490d941b566
Doron Rosenberg <doronr@us.ibm.com>
2016-03-31 15:16:28 -0700

    Update the remaining code to the new way

--- examples/dataframetest.js
+++ examples/dataframetest.js
@@ -19,7 +19,7 @@ var spark = require('../lib/index.js');
 var sparkContext = new spark.SparkContext("local[*]", "dataframe test");
 var sqlContext = new spark.SQLContext(sparkContext);
 
-var dataFrame = sqlContext.read.json("/tmp/examples/test.json");
+var dataFrame = sqlContext.read().json("/tmp/examples/test.json");
 //var gd = dataFrame.groupBy(dataFrame.col("first"));
 //var df2 = gd.count();
 dataFrame.show();
