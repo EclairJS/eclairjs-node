5ee30d69c8577ac05fdd121b91ee4be257424e0d
Doron Rosenberg <doronr@us.ibm.com>
2016-05-12 12:31:59 -0700

    fix #80 - Port ml Gradient Boosted Tree Regressor example

--- examples/ml/gradientboostedtreeregressor.js
+++ examples/ml/gradientboostedtreeregressor.js
@@ -37,12 +37,7 @@ function run(sc) {
       .format("libsvm")
       .load(__dirname+"/../mllib/data/sample_libsvm_data.txt");
 
-    // Index labels, adding metadata to the label column.
-    // Fit on whole dataset to include all labels in index.
-    var labelIndexer = new spark.ml.feature.StringIndexer()
-      .setInputCol("label")
-      .setOutputCol("indexedLabel")
-      .fit(data);
+
     // Automatically identify categorical features, and index them.
     // Set maxCategories so features with > 4 distinct values are treated as continuous.
     var featureIndexer = new spark.ml.feature.VectorIndexer()
@@ -55,42 +50,33 @@ function run(sc) {
     data.randomSplit([0.7, 0.3]).then(function(splits) {
       var trainingData = splits[0];
       var testData = splits[1];
-
+      
       // Train a GBT model.
-      var gbt = new spark.ml.classification.GBTClassifier()
-        .setLabelCol("indexedLabel")
+      var gbt = new spark.ml.regression.GBTRegressor()
+        .setLabelCol("label")
         .setFeaturesCol("indexedFeatures")
         .setMaxIter(10);
 
-      labelIndexer.labels().then(function(labels) {
-        // Convert indexed labels back to original labels.
-        var labelConverter = new spark.ml.feature.IndexToString()
-          .setInputCol("prediction")
-          .setOutputCol("predictedLabel")
-          .setLabels(labels);
+      // Chain indexer and GBT in a Pipeline
+      var pipeline = new spark.ml.Pipeline().setStages([featureIndexer, gbt]);
 
-        // Chain indexers and GBT in a Pipeline
-        var pipeline = new spark.ml.Pipeline()
-          .setStages([labelIndexer, featureIndexer, gbt, labelConverter]);
+      // Train model.  This also runs the indexer.
+      var model = pipeline.fit(trainingData);
 
-        // Train model.  This also runs the indexers.
-        var model = pipeline.fit(trainingData);
+      // Make predictions.
+      var predictions = model.transform(testData);
 
-        // Make predictions.
-        var predictions = model.transform(testData);
+      // Select (prediction, true label) and compute test error
+      var evaluator = new spark.ml.evaluation.RegressionEvaluator()
+        .setLabelCol("label")
+        .setPredictionCol("prediction")
+        .setMetricName("rmse");
 
-        // Select (prediction, true label) and compute test error
-        var evaluator = new spark.ml.evaluation.MulticlassClassificationEvaluator()
-          .setLabelCol("indexedLabel")
-          .setPredictionCol("prediction")
-          .setMetricName("precision");
+      var promises = [];
+      promises.push(predictions.select("prediction", "label", "features").count());
+      promises.push(evaluator.evaluate(predictions));
 
-        var promises = [];
-        promises.push(predictions.select("predictedLabel", "label", "features").count());
-        promises.push(evaluator.evaluate(predictions));
-
-        Promise.all(promises).then(resolve).catch(reject);
-      }).catch(reject)
+      Promise.all(promises).then(resolve).catch(reject);
     }).catch(reject);
   });
 }
@@ -99,10 +85,10 @@ if (global.SC) {
   // we are being run as part of a test
   module.exports = run;
 } else {
-  var sc = new spark.SparkContext("local[*]", "Gradient Boosted Tree Classifier");
+  var sc = new spark.SparkContext("local[*]", "Gradient Boosted Tree Regressor");
   run(sc).then(function(results) {
     console.log('Result count:', results[0]);
-    console.log('Test Error:', 1-results[1]);
+    console.log('Root Mean Squared Error (RMSE) on test data:', results[1]);
     stop();
   }).catch(stop);
 }
--- test/it/ml/ml.js
+++ test/it/ml/ml.js
@@ -196,6 +196,21 @@ describe('ml Test', function() {
     });
   });
 
+  describe("Gradient Boosted Tree Regressor", function() {
+    it("should return the expected result", function(done) {
+      this.timeout(100000);
+
+      var test = require('../../../examples/ml/gradientboostedtreeregressor');
+      test(sc).then(function(results) {
+        expect(results.length).equals(2);
+        expect(results[0]).to.be.an('Number');
+        expect(results[1]).to.be.an('Number');
+        done();
+      }).catch(done);
+    });
+  });
+
+
   after(function(done) {
     if (sc && doWeOwnTheSC) {
       sc.stop().then(done).catch(done);
