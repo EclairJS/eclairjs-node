361e2d3e742b1338c955b491541028f81a46638d
berkland@us.ibm.com <berkland@us.ibm.com>
2016-05-20 09:59:04 -0500

    add ml sql_transformer example and required apis

---
+++ examples/ml/sql_transformer.js
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2016 IBM Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+function exit() {
+  process.exit();
+}
+
+function stop(e) {
+  if (e) {
+    console.log(e);
+  }
+  sc.stop().then(exit).catch(exit);
+}
+
+var spark = require('../../lib/index.js');
+
+function run(sc) {
+  return new Promise(function(resolve, reject) {
+    var sqlContext = new spark.sql.SQLContext(sc);
+
+    // $example on$
+    var jrdd = sc.parallelize([
+      spark.sql.RowFactory.create([0, 1.0, 3.0]),
+      spark.sql.RowFactory.create([2, 2.0, 5.0])
+    ]);
+    var schema = new spark.sql.types.StructType( [
+      new spark.sql.types.StructField("id", spark.sql.types.DataTypes.IntegerType,
+          false, spark.sql.types.Metadata.empty()),
+      new spark.sql.types.StructField("v1", spark.sql.types.DataTypes.DoubleType,
+          false, spark.sql.types.Metadata.empty()),
+      new spark.sql.types.StructField("v2", spark.sql.types.DataTypes.DoubleType,
+          false, spark.sql.types.Metadata.empty())
+    ]);
+    var df = sqlContext.createDataFrame(jrdd, schema);
+
+    var sqlTrans = new spark.ml.feature.SQLTransformer().setStatement(
+      "SELECT *, (v1 + v2) AS v3, (v1 * v2) AS v4 FROM __THIS__");
+
+    var output=sqlTrans.transform(df);
+
+    output.take(10).then(resolve).catch(reject);
+
+  });
+}
+
+if (global.SC) {
+  // we are being run as part of a test
+  module.exports = run;
+} else {
+  var sc = new spark.SparkContext("local[*]", "sqlTransformer");
+  run(sc).then(function(results) {
+        console.log(JSON.stringify(results));
+    stop();
+  }).catch(stop);
+}
